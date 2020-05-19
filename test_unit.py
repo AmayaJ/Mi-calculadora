2b4cf8aabe0a04b94d5597ddbdf7e97cc1245b59
Amaya <amaya_jimenez@hotmail.com>
2020-05-19 22:31:15 +0200

    Creada la prueba de la funcion division con el Framework de pruebas Unittest

--- test_unit.py
+++ test_unit.py
@@ -1,5 +1,5 @@
 import unittest
-from calculadora import suma, resta
+from calculadora import suma, resta, multiplicacion
 
 class TestCalculadora(unittest.TestCase):
 
@@ -37,6 +37,42 @@ class TestCalculadora(unittest.TestCase):
 		resultadoEsperado = -8
 		#act
 		resultado = resta(valor1, valor2)
+		self.assertEqual(resultado, resultadoEsperado)
+
+	def test_multiplicacion(self):
+		#arrange
+		valor1 = 2
+		valor2 = 3
+		resultadoEsperado = 6
+		#act
+		resultado = multiplicacion(valor1, valor2)
+		self.assertEqual(resultado, resultadoEsperado)
+
+	def test_multiplicacion2(self):
+		#arrange
+		valor1 = -5
+		valor2 = 3
+		resultadoEsperado = -15
+		#act
+		resultado = multiplicacion(valor1, valor2)
+		self.assertEqual(resultado, resultadoEsperado)
+
+	def test_division(self):
+		#arrange
+		valor1 = -6
+		valor2 = 3
+		resultadoEsperado = -2
+		#act
+		resultado = division(valor1, valor2)
+		self.assertEqual(resultado, resultadoEsperado)
+
+	def test_division2(self):
+		#arrange
+		valor1 = 20
+		valor2 = 3
+		resultadoEsperado = 7,333333
+		#act
+		resultado = division(valor1, valor2)
 		self.assertEqual(resultado, resultadoEsperado)
 
 if __name__ == "__main__":
